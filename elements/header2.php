15403e5938c56dd9ba7a5539e5caddae228c0c93
AleisterFox <ericaguirrez@hotmail.com>
2024-04-05 10:34:46 -0600

    subida del proyecto

---
+++ elements/header2.php
@@ -0,0 +1,19 @@
+<header>
+      <nav>
+        <a href="index.php">
+          <picture>
+            <source media="(max-width: 768px)" srcset="img/logo.png" />
+            <img src="img/logo.png" alt="" />
+          </picture>
+        </a>
+
+        <ul class="menu">
+          <li><a class="active" href="index.php">Inicio</a></li>
+          <li><a href="us.php">Acerca de</a></li>
+          <li><a href="#services">Servicios</a></li>
+          <li><a href="contact.php">Contacto</a></li>
+          <li><a href="faqs.php" target="">Faq</a></li>
+        </ul>
+        <a><i class="fa-solid fa-bars menu__button"></i></a>
+      </nav>
+    </header>
