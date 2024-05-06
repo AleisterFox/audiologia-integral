216d5d65b60c2caaa209a47d35a9ec3778bcd24d
AleisterFox <ericaguirrez@hotmail.com>
2024-05-06 10:49:51 -0600

    cambios cdn iconos

--- index.php
+++ index.php
@@ -4,15 +4,8 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="https://cdn.jsdelivr.net/gh/eliyantosarage/font-awesome-pro@main/fontawesome-pro-6.5.1-web/css/all.min.css" rel="stylesheet" />
-  <link rel="stylesheet" href="css/style.css" />
-  <link rel="stylesheet" href="css/animate.css" />
-  <link rel="shortcut icon" href="img/icono.png" type="image/x-icon" media="(prefers-color-scheme: light)" />
-  <link rel="shortcut icon" href="img/icono.png" type="image/x-icon" media="(prefers-color-scheme: dark)" />
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css?family=Outfit:100,200,300,regular,500,600,700,800,900" rel="stylesheet" />
-  <link href="https://fonts.googleapis.com/css?family=Inter:100,200,300,regular,500,600,700,800,900" rel="stylesheet" />
+
+  <?php include 'elements/estilos.php'?>
 
   <link rel="stylesheet" href="css/loader.css">
 
